ef0c7abeea236dda36ebbe5df1b2a9f64a9d1a10
rubenbl <54916573+rubenbl@users.noreply.github.com>
2022-11-07 00:36:14 -0500

    Update employees.py

--- employees.py
+++ employees.py
@@ -4,13 +4,35 @@ import numpy as np
 
 st.title('Empleados')
 
+# ###############################################
+# Definición de funciones
+# ###############################################
 @st.cache
 def cargarDatos(nrows):
     data = pd.read_csv('Employees.csv', nrows=nrows)
     return data
 
-df = cargarDatos(500)
+def buscarEmpleadoPorID(texto):
+    df_tmp = data[data['Employee_ID'].str.upper().str.contains(texto)]
+    return df_tmp
 
-if st.sidebar.checkbox('Mostrar/ocultar el dataframe: '):
+# ###############################################
+# Main 
+# ###############################################
+
+df_empleados = cargarDatos(500)
+
+if st.sidebar.checkbox('Mostrar/ocultar el dataframe'):
     st.subheader('dataframe (employees)')
-    st.write(df)
+    st.write(df_empleados)
+
+inputEmpleadoID = st.sidebar.text_input('Busqueda por "Employee_ID" : ')
+btnBuscarEmpleadoID = st.sidebar.button('Buscar')
+
+if (btnBuscarEmpleadoID):
+   df_filtroEmpleadosID = buscarEmpleadoPorID(inputEmpleadoID.upper())
+   contador = df_filtroEmpleadosID.shape[0] 
+   st.write(f"Total de empleados identificados : {contador}")
+   st.write(df_filtroEmpleadosID)
+    
+   
