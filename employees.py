73199b530bf52ad8cb87dedb2f2f378fded3f80d
rubenbl <54916573+rubenbl@users.noreply.github.com>
2022-11-07 14:48:59 -0500

    Update employees.py

--- employees.py
+++ employees.py
@@ -181,4 +181,15 @@ if st.sidebar.checkbox('Mostrar/ocultar gráfico índice de deserción'):
     st.line_chart(df_empleadosResumen, y='Attrition_rate')
     st.dataframe(df_empleadosResumen)
     
+# Sección - Mostrar/ocultar gráfico edad vs. tasa de deserción
+if st.sidebar.checkbox('Mostrar/ocultar gráfico índice de deserción'):
+    st.subheader('Grafico')
+    st.write("\n16. Analizar la información con una gráfica que permita visualizar la edad y la tasa de deserción. ")
+    # Grafico: Seaborn
+    #employees[['Age','Time_of_service','Attrition_rate']].head(5) 
+    grafico = plt.figure(figsize=(10, 5))
+    sns.scatterplot(x=df_empleados['Age'] , y=df_empleados['Attrition_rate'])
+    st.pyplot(grafico)
     
+    
+
