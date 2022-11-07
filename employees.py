691294e5d76856348195ef9030718c4e28969403
rubenbl <54916573+rubenbl@users.noreply.github.com>
2022-11-07 12:28:10 -0500

    Update employees.py

--- employees.py
+++ employees.py
@@ -147,10 +147,10 @@ if st.sidebar.checkbox('Mostrar/ocultar gráfico índice de deserción'):
     st.subheader('Grafico')
     st.write("\n15. Analizar los datos con una gráfica que nos permita visualizar las ciudades (Hometown) que tienen el mayor índice de deserción. ")
     # Grafico: 
-    fig, axs = plt.subplots()
+    fig, axs = plt.subplots(subplots=True, layout=(2,5), figsize=(30,10))
     df_empleadosResumen = df_empleados.groupby(['Hometown']).mean()
     df_empleadosResumen.sort_values('Hometown', ascending=True)
-    axs = df_empleadosResumen.plot(subplots=True, layout=(2,5), figsize=(30,10)) 
+    axs = df_empleadosResumen.plot() 
     st.pyplot(fig)
 
     
