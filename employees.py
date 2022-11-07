6ee0be5840db47ab30e7761dcbfa26f4818c8534
rubenbl <54916573+rubenbl@users.noreply.github.com>
2022-11-07 12:33:27 -0500

    Update employees.py

--- employees.py
+++ employees.py
@@ -149,8 +149,8 @@ if st.sidebar.checkbox('Mostrar/ocultar gráfico índice de deserción'):
     # Grafico: 
     df_empleadosResumen = df_empleados.groupby(['Hometown']).mean()
     df_empleadosResumen = df_empleadosResumen.sort_values('Hometown', ascending=True)['Attrition_rate']
-    fig, axs = plt.subplots(figsize=(30,10))
-    axs = df_empleadosResumen.plot(subplots=True, layout=(2,5)) 
+    fig, ax = plt.subplots()
+    ax = df_empleadosResumen.plot(subplots=True, layout=(2,5),figsize=(30,10)) 
     st.pyplot(fig)
     
     
