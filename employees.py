1f1f77f69abc8f6b930bc1cd38bad5c14e69a0fc
rubenbl <54916573+rubenbl@users.noreply.github.com>
2022-11-07 00:51:25 -0500

    Update employees.py

--- employees.py
+++ employees.py
@@ -22,17 +22,24 @@ def buscarEmpleadoPorHometown(texto):
     df_tmp = df_empleados[df_empleados['Hometown'].str.upper().str.contains(texto)]
     return df_tmp
 
+@st.cache
+def buscarEmpleadoPorUnit(texto):
+    df_tmp = df_empleados[df_empleados['Unit'].str.upper().str.contains(texto)]
+    return df_tmp
 
 # ###############################################
 # Main 
 # ###############################################
 
+# Sección - Pre-carga de datos.
 df_empleados = cargarDatos(500)
 
+# Sección - Mostrar/ocultar dataframe
 if st.sidebar.checkbox('Mostrar/ocultar el dataframe'):
     st.subheader('dataframe (employees)')
     st.write(df_empleados)
 
+# Sección - Búsqueda por ID
 inputEmpleadoID = st.sidebar.text_input('Busqueda por "Employee_ID" : ')
 btnBuscarEmpleadoID = st.sidebar.button('Buscar ID')
 
@@ -41,7 +48,8 @@ if (btnBuscarEmpleadoID):
    contador = df_filtroEmpleadosID.shape[0] 
    st.write(f"Total de empleados identificados : {contador}")
    st.write(df_filtroEmpleadosID)
-    
+
+# Sección - Búsqueda por Hometown
 inputEmpleadoHometown = st.sidebar.text_input('Busqueda por "Hometown" : ')
 btnBuscarEmpleadoHometown = st.sidebar.button('Buscar Hometown')
 
@@ -50,3 +58,13 @@ if (btnBuscarEmpleadoHometown):
    contador = df_filtroEmpleadosHometown.shape[0] 
    st.write(f"Total de empleados identificados : {contador}")
    st.write(df_filtroEmpleadosHometown)
+
+# Sección - Búsqueda por Unit
+inputEmpleadoUnit = st.sidebar.text_input('Busqueda por "Unit" : ')
+btnBuscarEmpleadoUnit = st.sidebar.button('Buscar Unit')
+
+if (btnBuscarEmpleadoUnit):
+   df_filtroEmpleadosUnit = buscarEmpleadoPorUnit(inputEmpleadoUnit.upper())
+   contador = df_filtroEmpleadosUnit.shape[0] 
+   st.write(f"Total de empleados identificados : {contador}")
+   st.write(df_filtroEmpleadosUnit)
