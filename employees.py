5f3e0bde367693602a664665e30ed9ee5a96a5e8
rubenbl <54916573+rubenbl@users.noreply.github.com>
2022-11-07 14:08:00 -0500

    Update employees.py

--- employees.py
+++ employees.py
@@ -52,8 +52,11 @@ def buscarEmpleadoPorUnitCaseSensitive(texto):
 df_empleados = cargarDatos(500)
 
 st.sidebar.title("Búsqueda interactiva de empleados")
-st.sidebar.text("Actividad : DSA05 - Reto ")
-st.sidebar.text("Autor : Ruben Barrios")
+st.sidebar.markdown("""
+Actividad : DSA05 - Reto 
+\nAutor : Ruben Barrios
+""")
+st.sidebar.text("-------------------------")
 
 # Sección - Mostrar/ocultar dataframe
 if st.sidebar.checkbox('Mostrar/ocultar el dataframe'):
